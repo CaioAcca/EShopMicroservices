6e57c771bb107b269b36fe838839399e63994a92
caio.accati <caio.accati@gmail.com>
2025-12-20 12:01:14 -0300

    feat: adicionado endpoint para remover produto

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool isSuccess);
+
+internal class DeleteProductHandler
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductHandler.Handle called with {@Command}", command);
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync();
+
+        return new DeleteProductResult(true);
+    }
+}
